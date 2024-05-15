e0d2207b08f4a21150bec369ddc83a4ade3085cd
Cristobal Rosa <crisrogar@gmail.com>
2024-05-15 09:04:49 +0200

    check for a tie

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,10 @@ function App() {
     setWinner(null)
   }
 
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null)
+  }
+
   const updateBoard = (index) => {
     if (board[index] || winner)  return
 
@@ -72,6 +76,8 @@ function App() {
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
       setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
     }
   }
 
